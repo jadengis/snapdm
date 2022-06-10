4e1c89f058f2d651220363b0b3937c2b2ee31dd4
John Dengis <jadengis@gmail.com>
2022-06-09 20:29:47 -0700

    run migrations and stuff

--- packages/model/jest.config.ts
+++ packages/model/jest.config.ts
@@ -1,15 +1,17 @@
-module.exports = {
-  displayName: 'preconditions',
+/* eslint-disable */
+export default {
+  displayName: 'model',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
     },
   },
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/preconditions',
+  coverageDirectory: '../../coverage/packages/model',
   testEnvironment: 'node',
 };
--- packages/preconditions/jest.config.ts
+++ packages/preconditions/jest.config.ts
@@ -1,16 +1,16 @@
-module.exports = {
-  displayName: 'model',
+/* eslint-disable */
+export default {
+  displayName: 'preconditions',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
     },
   },
-  setupFiles: ['<rootDir>/src/test-setup.ts'],
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/model',
+  coverageDirectory: '../../coverage/packages/preconditions',
   testEnvironment: 'node',
 };
