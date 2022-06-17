f63bb49f0c90e4050e3aaee8d25909da9ad5bae2
John Dengis <jadengis@gmail.com>
2022-06-16 21:05:40 -0700

    fix: merging a field with empty array update doesn't overwrite. (#5)
    Adds a failing test for this case and introduces a fix.

--- packages/model/src/lib/utils/merge.ts
+++ packages/model/src/lib/utils/merge.ts
@@ -1,4 +1,4 @@
-import __merge from 'lodash/merge';
+import __mergeWith from 'lodash/mergeWith';
 
 export function merge<T1, T2>(t1: T1, t2: T2): T1 & T2;
 export function merge<T1, T2, T3>(t1: T1, t2: T2, t3: T3): T1 & T2 & T3;
@@ -9,5 +9,7 @@ export function merge<T1, T2, T3, T4>(
   t4: T4
 ): T1 & T2 & T3 & T4;
 export function merge(...sources: unknown[]): unknown {
-  return __merge({}, ...sources);
+  return __mergeWith({}, ...sources, (obj: unknown, val: unknown) =>
+    Array.isArray(obj) ? val : undefined
+  );
 }
